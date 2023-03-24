60b330473544961d66b924e146993bf3957bde38
ofplarsen <ofplarsen@gmail.com>
2023-03-24 11:39:24 +0100

    Small program for receiving stream, doing cca and returning a value

--- jitter/main.py
+++ jitter/main.py
@@ -1,85 +1,92 @@
-import pyxdf
-import pandas as pd
-import matplotlib.pyplot as plt
+import time
+
 import numpy as np
+import pandas as pd
+from numpy.random import rand
+from pylsl import StreamInlet, resolve_stream, StreamInfo, StreamOutlet, pylsl, local_clock
+from sklearn.cross_decomposition import CCA
 
-class Blink:
-    def __init__(self, timestamp, index):
-        self.timestamp = timestamp
-        self.index = index
-
-
-def get_time_stamps(stamps: list):
-    start = []
-    end = []
-    more_than_one = False
-
-    for i in range(len(stamps)):
-        if i == (len(stamps)-1):
-            break
-
-        if(stamps[i] == (stamps[i+1] - 1)):
-            if not more_than_one:
-                start.append(stamps[i])
-            more_than_one = True
-            continue
-        else:
-            if more_than_one:
-                end.append(stamps[i])
-                more_than_one = False
-
-    return start, end
-
-
-data, header = pyxdf.load_xdf('data/data18022023/eeg-and-eyetracking.xdf')
-eeg, header_egg = pyxdf.load_xdf('data/LSLDataV1/LSLData2/LSLData/raw-eeg.xdf')
-
-left_blinks = None
-right_blinks = None
-both_blink = None
-for stream in data:
-    y = stream['time_series']
-    blinking_df = pd.DataFrame(y)
-    blinking_df.rename(index={3: "left", 4: "right", 5: "both"})
-    blinking_df.drop([0, 1, 2], inplace=True, axis=1)
-
-    print(blinking_df)
-    left_blinks = blinking_df.index[(blinking_df[3] >= 1) & (blinking_df[4] != 1)].tolist()
-    right_blinks = blinking_df.index[(blinking_df[4] >= 1) & (blinking_df[3] != 1)].tolist()
-    both_blink = blinking_df.index[blinking_df[5] == 1].tolist()
-    print(left_blinks)
-    pass
+help_string = 'SendData.py -s <sampling_rate> -n <stream_name> -t <stream_type>'
 channels = ['Fp1', 'Fz', 'F3', 'F7', 'F9', 'FC5', 'FC1', 'C3', 'T7', 'CP5', 'CP1', 'Pz', 'P3', 'P7'
             , 'P9', 'O1', 'Oz', 'O2', 'P10', 'P8', 'P4', 'CP2', 'CP6', 'T8', 'C4', 'Cz'
-            , 'FC2', 'FC6', 'F10', 'F8', 'F4', 'Fp2', 'AUX_1', 'AUX_2']
+            , 'FC2', 'FC6', 'F10', 'F8', 'F4', 'Fp2', 'ACC_X', 'ACC_Y', 'ACC_Z']
 removed_channels = ['Fp1', 'F8', 'F7', 'Fp2', 'F3', 'F4']
+frequencies_main = ['8.18', '9', '10', '11.25', '12.85', '15']
+frequencies = ['8.18_sin_h1','8.18_cos_h1','8.18_sin_h2','8.18_cos_h2','8.18_sin_h3','8.18_cos_h3',
+               '9_sin_h1', '9_cos_h1','9_sin_h2', '9_cos_h2','9_sin_h3', '9_cos_h3',
+               '10_sin_h1','10_cos_h1','10_sin_h2','10_cos_h2','10_sin_h3','10_cos_h3',
+               '11.25_sin_h1','11.25_cos_h1','11.25_sin_h2','11.25_cos_h2','11.25_sin_h3','11.25_cos_h3',
+               '12.85_sin_h1','12.85_cos_h1','12.85_sin_h2','12.85_cos_h2','12.85_sin_h3','12.85_cos_h3',
+               '15_sin_h1', '15_cos_h1', '15_sin_h2', '15_cos_h2', '15_sin_h3', '15_cos_h3'
+               ]
+fs = 250  # Sampling frequency
+fragment_duration = 5  # Fragment duration in seconds
+fragment_samples = fs * fragment_duration
+
+# Create an LSL stream
+stream_name = 'CCA'
+stream_type = 'cca'
+channel_count = 1
+sampling_rate = 0  # Irregular sampling rate (use a positive number for a regular sampling rate)
+channel_format = 'float32'
+
+# Create the stream info object
+info = StreamInfo(stream_name, stream_type, channel_count, sampling_rate, channel_format, 'myuid34234')
+
+# Create the LSL outlet
+outlet = StreamOutlet(info)
 
 
-start, end = get_time_stamps(right_blinks)
-print(start, end)
-rang = 50
-for stream in eeg:
-    for j in range(len(start)):
-        y = stream['time_series']
-        blinking_df = pd.DataFrame(y)
-        blinking_df = blinking_df[start[j] - rang:end[j] + rang]
-        #print(blinking_df.info())
-        blinking_y = blinking_df.to_numpy()
-        index = 0
-        for i in range(32):
-            index += 1
-            if channels[i] in removed_channels:
-                index -= 1
-            else:
-                axis = plt.subplot(7, 4, index)
-                axis.axvline(stream['time_stamps'][start[j]], color='red')  # y = 0
-                axis.axvline(stream['time_stamps'][end[j]], color='red')
-                axis.plot(stream['time_stamps'][start[j]-rang:end[j]+rang], blinking_df[blinking_df.columns[i]])
-                axis.set_title(channels[i])
-
-        plt.show()
+# Resolve an LSL stream on the network
+print("Looking for an LSL stream...")
+streams = resolve_stream('type', 'Test')
+inlet = StreamInlet(streams[0])
 
 
+def perform_cca(fragment, n_components):
+    X = fragment[:][['O1', 'O2', 'Oz', 'P3', 'P4', 'Pz', 'P7', 'P8']]
+    freqs = []
+    t = 0
+    for i in range(0, len(frequencies), 6):
+        t = t + 1
+        Y = fragment[:][frequencies[i:6 * t]]
+        #print(Y.shape)
+        ca = CCA(n_components=1)
+        ca.fit(X, Y)
+        X_c, Y_c = ca.transform(X, Y)
+        #print([np.corrcoef(X_c[:, i], Y_c[:, i])[0, 1] for i in range(1)])
+        freqs.append(np.corrcoef(X_c[:, 0], Y_c[:, 0])[0][1])
+    return freqs
 
+def return_index(index, info, outlet):
+    # Send a single value
+    value = float(index)
+    timestamp = time.time()
+    outlet.push_sample([value], timestamp)
 
+buffer = []
 
+print("now sending data...")
+
+while True:
+    # Receive data from the LSL stream
+    sample, timestamp = inlet.pull_sample()
+
+    # Append the received sample to the buffer
+    buffer.append(sample)
+    val = -1
+    if len(buffer) >= fragment_samples:
+        # Convert the buffer to a NumPy array and reshape it
+        fragment = np.array(buffer[:fragment_samples])
+
+        # Process the 5-second fragment here (e.g., apply a filter, analyze, or save it)
+        print("Received a 5-second fragment:", fragment)
+        # Remove the processed fragment from the buffer
+        buffer = buffer[fragment_samples:]
+        df = pd.DataFrame(fragment)
+        df.columns = frequencies + channels
+        cca = perform_cca(df,1)
+        print(cca)
+        val = np.argmax(cca)
+        print(val)
+        return_index(val, info, outlet)
