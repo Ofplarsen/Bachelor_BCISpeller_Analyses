1131aaf00c3acf5276bb274b09ffbe259d6fb47f
ofplarsen <ofplarsen@gmail.com>
2023-03-29 11:19:34 +0200

    Added second stream

--- jitter/ BCISpeller/MockEEG.py
+++ jitter/ BCISpeller/MockEEG.py
@@ -2,23 +2,33 @@ import numpy as np
 import time
 from pylsl import StreamInfo, StreamOutlet
 
-# Define the LSL stream parameters
-num_channels = 35
-sampling_rate = 250
+# Define the LSL stream parameters for the EEG stream
+num_channels = 8
+sampling_rate = 500
 channel_format = 'float32'
-stream_name = 'MockEEG'
-stream_type = 'EEG'
+eeg_stream_name = 'MockEEG'
+eeg_stream_type = 'EEG'
 
-# Create the LSL stream info and outlet
-info = StreamInfo(stream_name, stream_type, num_channels, sampling_rate, channel_format)
-outlet = StreamOutlet(info)
+# Create the LSL stream info and outlet for the EEG stream
+eeg_info = StreamInfo(eeg_stream_name, eeg_stream_type, num_channels, sampling_rate, channel_format)
+eeg_outlet = StreamOutlet(eeg_info)
+
+# Define the LSL stream parameters for the single sample stream
+sample_stream_name = 'MockN'
+sample_stream_type = 'MockN'
+sample_channels = 1
+
+# Create the LSL stream info and outlet for the single sample stream
+sample_info = StreamInfo(sample_stream_name, sample_stream_type, sample_channels, sampling_rate, channel_format)
+sample_outlet = StreamOutlet(sample_info)
 
 # Define the signal generation parameters
-duration = 6  # Duration of each segment of the signal (seconds)
+duration = 7  # Duration of each segment of the signal (seconds)
 base_frequency = 24  # Frequency of the sinusoidal components (Hz)
-noise_amplitude = 0.5  # Amplitude of the random noise component
+noise_amplitude = 50  # Amplitude of the random noise component
+sample_value = 0  # The value N for the single sample stream
 
-# Continuously generate and stream mock EEG data
+# Continuously generate and stream mock EEG data and the single sample value
 while True:
     t = np.arange(0, duration, 1/sampling_rate)
     eeg_data = []
@@ -32,5 +42,6 @@ while True:
 
     # Stream the generated mock EEG data
     for sample in eeg_data:
-        outlet.push_sample(sample)
-        time.sleep(1/sampling_rate)
+        sample_value = sample_value + 1
+        eeg_outlet.push_sample(sample)
+        sample_outlet.push_sample([sample_value])  #
